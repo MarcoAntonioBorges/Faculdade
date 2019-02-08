4f0ff5d48355019e3dc82dcb6985e4b01673d533
MarcoAntonioBorges <mabo.marcoantonio@gmail.com>
2019-02-08 09:42:14 -0200

    Criação de Anotação Java

---
+++ ENTERPRISE APPLICATION DEVELOPMENT/Projetos/01-Revisao/src/br/com/fiap/dao/CarroDAOMySQL.java
@@ -0,0 +1,25 @@
+package br.com.fiap.dao;
+
+import java.util.List;
+
+import br.com.fiap.beans.Carro;
+
+//Uma classe pode herdar apenas de uma classe
+//Uma classe pode implementar uma ou mais interfaces
+public class CarroDAOMySQL implements CarroDAO{
+
+	@Override
+	public List<Carro> buscarCarros() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void criarNeh(Carro carro) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
---
+++ ENTERPRISE APPLICATION DEVELOPMENT/Projetos/02-Anotacoes/src/br/com/fiap/anotacao/Tabela.java
@@ -0,0 +1,14 @@
+package br.com.fiap.anotacao;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE) // ElementType.FIELD restringe a anotação somente para atributos
+public @interface Tabela {
+
+	String nome();
+	
+}
