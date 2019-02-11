36a41e638ae5b2cd51d916a594315a0cd321fe7a
MarcoAntonioBorges <mabo.marcoantonio@gmail.com>
2019-02-11 10:48:26 -0200

    Atualizando arquivos Android

--- DIGITAL BUSINESS ENABLEMENT/workspace/01-Loja-App/src/br/com/fiap/loja/TerminalConsulta/Teste.java
+++ DIGITAL BUSINESS ENABLEMENT/workspace/01-Loja-App/src/br/com/fiap/loja/TerminalConsulta/Teste.java
@@ -4,12 +4,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
+import br.com.fiap.loja.bo.EstoqueBO;
+
 public class Teste {
 	@SuppressWarnings("all")
 	public static void main(String[] args) {
 		
 		String[] camisetas = new String[]{"Camiseta branca", "Camiseta Azul", "Camiseta Rosa"};
-		
+		String nomeLoja = "FIAP";
 		LocalDate hoje = LocalDate.now();
 		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		int cod = 0;
@@ -17,27 +19,16 @@ public class Teste {
 		Scanner teclado = new Scanner(System.in);
 		
 		System.out.println();
-		System.out.println("----- Bem Vindo! Data Atual: " + hoje.format(formatador));
+		System.out.println("----- " + nomeLoja + " Bem Vindo! Data Atual: " + hoje.format(formatador));
 		System.out.println();
 		
 		
 		System.out.print("Digite o codigo: ");
 		cod = teclado.nextInt();
 		
-		switch (cod) {
-			case 401 :
-				System.out.println(camisetas[0]);
-			break;
-			case 402 :
-				System.out.println(camisetas[1]);
-				break;
-			case 403:
-				System.out.println(camisetas[2]);
-				break;
-			default:
-				System.out.println("Produto não encontrado");
-				break;
-		}
+		System.out.println(EstoqueBO.consultaProduto(cod).getAll());
+		
+		teclado.close();
 		
 	}
 }
